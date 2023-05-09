904cad9821e7045ae72f982c4a5ca5e4e1c7aced
levanthien1812 <20521947@gm.uit.edu.vn>
2023-05-09 09:44:09 +0700

    add connection to db

---
+++ src/ConnectDB/connect_db.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConnectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author THIEN
+ */
+public class connect_db {
+    public static Connection getConnection() throws ClassNotFoundException,  
+            SQLException{
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        String connectionURL = "jdbc:mysql://localhost:3306/charity_management_app";
+        String username = "root";
+        String password = "charityapp";
+
+        Connection conn = DriverManager.getConnection(connectionURL, username,
+                password);
+        return conn;
+    }
+}
